4f8f5485aa13bd2102b828b336896f4e24ef27c3
jaygim <kimjs@hackle.io>
2021-07-12 23:41:47 +0900

    [3장] 요구 사항 1완료

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,9 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,7 +24,12 @@ public class RequestHandler extends Thread {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
+            String line = bufferedReader.readLine();
+            String url = line.split(" ")[1];
+
+            byte[] body = Files.readAllBytes(Paths.get("webapp", url));
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
